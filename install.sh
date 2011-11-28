0a2b0957cfece56599be8cab0dc0b076e358c4f9
yvonne yip <ykyyip@gmail.com>
2011-11-27 21:45:43 -0800

    update rsync filter

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-rsync -varuP --exclude="*wp" --exclude=".git" $1 /Applications/Live\ 8.2.6/Live.app/Contents/App-Resources/MIDI\ Remote\ Scripts/
+rsync -varuP --exclude="*wp" --exclude="*wo" --exclude=".git" $1 /Applications/Live\ 8.2.6/Live.app/Contents/App-Resources/MIDI\ Remote\ Scripts/
